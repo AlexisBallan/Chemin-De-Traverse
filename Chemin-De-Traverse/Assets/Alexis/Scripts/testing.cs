692746093400a6fd9712c19137511064845941de
Alexis <allexis.bal@hotmail.fr>
2021-11-08 20:07:43 -0500

    Edit grid

---
+++ Chemin-De-Traverse/Assets/Alexis/Scripts/testing.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class testing : MonoBehaviour
+{
+
+    public int ligne, column;
+    private void Start()
+    {
+        Grid grid = new Grid(ligne, column);
+    }
+}
